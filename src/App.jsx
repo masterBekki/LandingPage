b8fe70f29d71ac645df2c3d49c19487c4b6b88c5
masterBekshoh <bekshohdeveloper@gmail.com>
2023-12-03 11:51:51 +0500

    components update

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">
-    Master 
-    <span>Master branchi</span>
-  </h1>;
-}
-export default App
---
+++ src/components/app/App.jsx
@@ -0,0 +1,12 @@
+import Hero from '../hero/hero';
+import Nav from '../nav/nav';
+import './App.css'
+function App() {
+  return (
+    <div className='container'>
+      <Nav/>
+      <Hero/>
+    </div>
+  );
+}
+export default App
